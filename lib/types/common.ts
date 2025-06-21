6ac2c7e38aa45ecac0a1eaaf182e738149645fcc
Matthew Quinn <iam@matthewquinn.me>
2025-06-21 21:11:36 +1200

    Adds common JSON types

---
+++ lib/types/common.ts
@@ -0,0 +1,28 @@
+
+// deno-lint-ignore ban-types
+export type EmptyObject = {};
+
+export type JSONPrimitive = string | number | boolean | null | undefined;
+
+export type JSONValue = JSONPrimitive | JSONObject | JSONArray;
+
+export type JSONObject = { [member: string]: JSONValue };
+
+export interface JSONArray extends Array<JSONValue> {}
+
+export type IRIObject<Properties extends JSONObject = JSONObject> =
+  & Properties
+  & {
+    '@id': string;
+  };
+
+export type TypeObject<Properties extends JSONObject = JSONObject> =
+  & Properties
+  & {
+    '@type': string | string[];
+  };
+
+export type IterableJSONLD<Properties extends JSONObject = JSONObject> =
+  | JSONArray
+  | (Properties & { '@list': JSONArray })
+  | (Properties & { '@set': JSONArray });
